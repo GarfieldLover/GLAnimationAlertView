fab043701ebea8b599ad96635b6715c49fb01a30
张科 <zhangke19880503@163.com>
2017-11-27 18:34:36 +0800

    加入文件，再处理动画

---
+++ GLAnimationAlertView/GLAnimationAlertView/GLAnimationAlertView.h
@@ -0,0 +1,22 @@
+//
+//  GLAnimationAlertView.h
+//  GLAnimationAlertView
+//
+//  Created by ZK on 2017/11/27.
+//  Copyright © 2017年 ZK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GLAnimationAlertView.
+FOUNDATION_EXPORT double GLAnimationAlertViewVersionNumber;
+
+//! Project version string for GLAnimationAlertView.
+FOUNDATION_EXPORT const unsigned char GLAnimationAlertViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GLAnimationAlertView/PublicHeader.h>
+
+
+#import "UIView+Sizes.h"
+#import "WKAlertView.h"
+
---
+++ GLAnimationAlertViewDemo/GLAnimationAlertViewDemo/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  GLAnimationAlertViewDemo
+//
+//  Created by ZK on 2017/11/27.
+//  Copyright © 2017年 ZK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLAnimationAlertView/GLAnimationAlertView.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
